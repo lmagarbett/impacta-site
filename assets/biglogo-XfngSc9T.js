f811f65269a835afb500e1a138f21d772d58d568
lmagarbett <161760995+lmagarbett@users.noreply.github.com>
2025-10-01 15:57:16 +0100

    Updates

---
+++ assets/biglogo-XfngSc9T.js
@@ -0,0 +1,1 @@
+const o="/impacta-site/assets/biglogo-DhVnC_eB.png";export{o as b};
---
+++ assets/staff2-DmZwrfeF.js
@@ -0,0 +1,1 @@
+const s="/impacta-site/assets/staff1-j7JWzFtO.jpg",t="/impacta-site/assets/staff2-CdP3Q7GH.png";export{t as a,s};
